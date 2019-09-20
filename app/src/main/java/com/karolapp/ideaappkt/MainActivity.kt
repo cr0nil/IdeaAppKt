ba2e4296e1d328a8e3caabbff5fef57a200f2cb7
Karol <karlkurt1996@gmail.com>
2019-09-20 10:20:41 +0200

    add welcome screen

---
+++ app/src/main/java/com/karolapp/ideaappkt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.karolapp.ideaappkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    var TAG = "EntranceActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
