812b7d2d1178b33cc6dc4cf8558ad27d3611e81d
Karol <karlkurt1996@gmail.com>
2019-12-29 15:18:48 +0100

    unit testing email validation

---
+++ app/src/test/java/com/karolapp/ideaappkt/ui/presenter/handlers/RegisterPreseterImplTest.kt
@@ -0,0 +1,12 @@
+package com.karolapp.ideaappkt.ui.presenter.handlers
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RegisterPreseterImplTest {
+
+    @Test
+    fun validEmail() {
+    }
+}
